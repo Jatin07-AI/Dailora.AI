4cd49f1b48b2571c4c63ad41f0b8fddffba72acd
jatin06-code <jatin.p@codiste.com>
2025-09-17 16:03:32 +0530

    "Explicit time changed"

--- src/main/java/webDriverUtility/WebDriverUtilityProgram.java
+++ src/main/java/webDriverUtility/WebDriverUtilityProgram.java
@@ -35,7 +35,7 @@ public class WebDriverUtilityProgram {
 	}
 	
 	public void elementTobeClickable(WebDriver driver,WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 		
 	}
