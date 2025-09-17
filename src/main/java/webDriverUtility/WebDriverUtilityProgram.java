50b82294380ac998ff176389548b705aed52d430
jatin06-code <jatin.p@codiste.com>
2025-09-17 16:08:13 +0530

    "Explicit wait changed once again"

--- src/main/java/webDriverUtility/WebDriverUtilityProgram.java
+++ src/main/java/webDriverUtility/WebDriverUtilityProgram.java
@@ -35,7 +35,7 @@ public class WebDriverUtilityProgram {
 	}
 	
 	public void elementTobeClickable(WebDriver driver,WebElement element) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 		
 	}
