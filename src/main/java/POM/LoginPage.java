4d82943071e4ce799d1cffdb5faeb5c764673e46
jatin06-code <jatin.p@codiste.com>
2025-09-18 13:07:54 +0530

    "In dev"

--- src/main/java/POM/LoginPage.java
+++ src/main/java/POM/LoginPage.java
@@ -1,5 +1,4 @@
 package POM;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
